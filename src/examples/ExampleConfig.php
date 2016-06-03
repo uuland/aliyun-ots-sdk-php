f233ae55b6a9ffb657915a40a21f67e6faa2f464
redchen <redchen1255@gmail.com>
2016-06-03 14:39:11 +0800

    init the sdk.

---
+++ src/examples/ExampleConfig.php
@@ -0,0 +1,7 @@
+<?php
+
+
+define('EXAMPLE_END_POINT', 'http://ay45w-office.ots.aliyun-inc.com');
+define('EXAMPLE_ACCESS_KEY_ID', 'bbc07W6jDi214noU');
+define('EXAMPLE_ACCESS_KEY_SECRET', 'Uov2JPb9KADgAaXKnILJQwnVSd0Lhb');
+define('EXAMPLE_INSTANCE_NAME', 'wanhong-public');
