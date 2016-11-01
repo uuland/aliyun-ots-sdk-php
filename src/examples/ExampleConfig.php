2125bb37ac9ea25d9d2550ef0fb1fdb946c9b75a
red-chen <redchen1255@gmail.com>
2016-11-01 10:25:21 +0800

    Update ExampleConfig.php

--- src/examples/ExampleConfig.php
+++ src/examples/ExampleConfig.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('EXAMPLE_END_POINT', 'http://ay45w-office.ots.aliyun-inc.com');
-define('EXAMPLE_ACCESS_KEY_ID', 'bbc07W6jDi214noU');
-define('EXAMPLE_ACCESS_KEY_SECRET', 'Uov2JPb9KADgAaXKnILJQwnVSd0Lhb');
-define('EXAMPLE_INSTANCE_NAME', 'wanhong-public');
+define('EXAMPLE_END_POINT', 'http://');
+define('EXAMPLE_ACCESS_KEY_ID', 'accessid');
+define('EXAMPLE_ACCESS_KEY_SECRET', 'accesskey');
+define('EXAMPLE_INSTANCE_NAME', 'instance');
