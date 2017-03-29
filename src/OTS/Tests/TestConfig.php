066a8c636360921df1f0af5cd0934e6421a2efab
moyo <git@moyo.im>
2017-03-29 14:35:10 +0800

    Remove unuseful files for composer repo

--- src/OTS/Tests/TestConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-define('SDK_TEST_END_POINT', '');
-define('SDK_TEST_ACCESS_KEY_ID', '');
-define('SDK_TEST_ACCESS_KEY_SECRET', '');
-define('SDK_TEST_INSTANCE_NAME', '');
--- src/examples/ExampleConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('EXAMPLE_END_POINT', 'http://');
-define('EXAMPLE_ACCESS_KEY_ID', 'accessid');
-define('EXAMPLE_ACCESS_KEY_SECRET', 'accesskey');
-define('EXAMPLE_INSTANCE_NAME', 'instance');
